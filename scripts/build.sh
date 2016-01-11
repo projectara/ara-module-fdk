0f5b7d7b02db3bfdfa7c14bd92cf2ff1b0a0925b
Mike Mogenson <michael.mogenson@leaflabs.com>
2016-01-11 14:55:47 -0500

    FDK: Moved buid_ara_image functionality into build.sh
    Moved build_image_from_defconfig() and copy_image_files() into build.sh so the
    FDK can manage it's own module builds and not be susceptible to future changes
    in buid_ara_image.sh
    
    Testing Done: ran sucessful builds
    Bug Number: SW-2166
    Review URL: https://reviews.leaflabs.com/r/2535/
    Reviewed by: Joel Porquet
    
    Signed-off-by: Michael Mogenson <michael.mogenson@leaflabs.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -27,14 +27,105 @@
 #
 # Author: Fabien Parent <fparent@baylibre.com>
 
+# define exit error codes
+ARA_BUILD_CONFIG_ERR_BAD_PARAMS=1
+ARA_BUILD_CONFIG_ERR_NO_NUTTX_TOPDIR=2
+ARA_BUILD_CONFIG_ERR_CONFIG_NOT_FOUND=3
+ARA_BUILD_CONFIG_ERR_CONFIG_COPY_FAILED=4
 
-buildname=$BUILDNAME
-defconfigFile=$OOT_CONFIG
-buildbase=$BUILDBASE
-configpath=$SCRIPTPATH
+# Other build configuration.
+ARA_MAKE_PARALLEL=1            # controls make's -j flag
+ARA_MAKE_ALWAYS=""             # controls make's -B (--always-make) flag
 
-cd $NUTTX_ROOT
-source build_ara_image.sh
-build_image_from_defconfig
-copy_image_files
-exit $MAKE_RESULT
+build_image_from_defconfig() {
+  # configpath, defconfigFile, buildname, buildbase
+  # must be defined on entry
+
+  echo "Build config file   : $defconfigFile"
+  echo "Build name          : '$buildname'"
+
+  # define paths used during build process
+  ARA_BUILD_CONFIG_PATH="$buildbase/$buildname/config"
+  ARA_BUILD_IMAGE_PATH="$buildbase/$buildname/image"
+  ARA_BUILD_TOPDIR="$buildbase/$buildname"
+
+  echo "Build output folder : $ARA_BUILD_TOPDIR"
+  echo "Image output folder : $ARA_BUILD_IMAGE_PATH"
+
+  # delete build tree if it already exists
+  if [ -d $ARA_BUILD_TOPDIR ] ; then
+    rm -rf $ARA_BUILD_TOPDIR
+  fi
+
+  # create folder structure in build output tree
+  mkdir -p "$ARA_BUILD_CONFIG_PATH"
+  mkdir -p "$ARA_BUILD_IMAGE_PATH"
+  mkdir -p "$ARA_BUILD_TOPDIR"
+
+  # Copy nuttx tree to build tree
+  cp -r $NUTTX_ROOT/nuttx $ARA_BUILD_TOPDIR/nuttx
+  cp -r $NUTTX_ROOT/apps $ARA_BUILD_TOPDIR/apps
+  cp -r $NUTTX_ROOT/misc $ARA_BUILD_TOPDIR/misc
+
+  pushd $ARA_BUILD_TOPDIR/nuttx > /dev/null
+
+  make distclean
+
+  # copy Make.defs to build output tree
+  if ! install -m 644 -p ${configpath}/Make.defs ${ARA_BUILD_TOPDIR}/nuttx/Make.defs  >/dev/null 2>&1; then
+      echo "Warning: Failed to copy Make.defs"
+  fi
+
+  # copy setenv.sh to build output tree
+  if  install -p ${configpath}/setenv.sh ${ARA_BUILD_TOPDIR}/nuttx/setenv.sh >/dev/null 2>&1; then
+  chmod 755 "${ARA_BUILD_TOPDIR}/nuttx/setenv.sh"
+  fi
+
+  # copy defconfig to build output tree
+  if ! install -m 644 -p ${defconfigFile} ${ARA_BUILD_TOPDIR}/nuttx/.config ; then
+      echo "ERROR: Failed to copy defconfig"
+      exit $ARA_BUILD_CONFIG_ERR_CONFIG_COPY_FAILED
+  fi
+
+  # save config files
+  cp ${ARA_BUILD_TOPDIR}/nuttx/.config   ${ARA_BUILD_CONFIG_PATH}/.config > /dev/null 2>&1
+  cp ${ARA_BUILD_TOPDIR}/nuttx/Make.defs ${ARA_BUILD_CONFIG_PATH}/Make.defs > /dev/null 2>&1
+  cp ${ARA_BUILD_TOPDIR}/nuttx/setenv.sh  ${ARA_BUILD_CONFIG_PATH}/setenv.sh > /dev/null 2>&1
+
+  echo -n "Building '$buildname'" ...
+  export ARA_BUILD_NAME=$buildname
+  make  -j ${ARA_MAKE_PARALLEL} ${ARA_MAKE_ALWAYS} -r -f Makefile.unix  2>&1 | tee $ARA_BUILD_TOPDIR/build.log
+
+  MAKE_RESULT=${PIPESTATUS[0]}
+
+  popd > /dev/null
+}
+
+copy_image_files() {
+  echo "Copying image files"
+  imgfiles="nuttx nuttx.bin System.map"
+  for fn in $imgfiles; do
+    cp $ARA_BUILD_TOPDIR/nuttx/$fn $ARA_BUILD_TOPDIR/image/$fn  >/dev/null 2>&1
+    rm -f $ARA_BUILD_TOPDIR/nuttx/$fn >/dev/null 2>&1
+  done
+  # if bridge image (i.e. *not* an svc image)
+  # expand image to 2M using truncate utility
+  # for more info, run "truncate --help"
+  if [ -z $(echo $buildname | grep "svc")  ] ; then
+    truncate -s 2M $ARA_BUILD_TOPDIR/image/nuttx.bin
+  fi
+}
+
+main() {
+  buildname=$BUILDNAME
+  defconfigFile=$OOT_CONFIG
+  buildbase=$BUILDBASE
+  configpath=$SCRIPTPATH
+
+  cd $NUTTX_ROOT
+  build_image_from_defconfig
+  copy_image_files
+  exit $MAKE_RESULT
+}
+
+main
