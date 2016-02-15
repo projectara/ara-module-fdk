projectara/ara-module-fdk

commit 7e0502d9e425c6ebf4e2ed275261b26da0ecb75d

white-camera: Update to the latest NuttX camera capabilities API
The .get_required_size() operation has been dropped in favour of
returning the capabilities size through the .get_capabilities()
operation only, and the outdated capabilities macro have been dropped.
Update the driver accordingly.

Testing Done: Tested on EVT1.2 with white camera module
Bug Number: SW-2915
Review URL: https://reviews.leaflabs.com/r/2940/
Reviewed by: fparent

Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

module/white-camera/board.c
@@ -1030,38 +1030,17 @@ static int ov5645_configure(struct sensor_info *info,
 /**
  * @brief Get capabilities of camera module
  * @param dev Pointer to structure of device data
- * @param capabilities Pointer that will be stored Camera Module capabilities.
+ * @param caps Pointer that will be stored Camera Module capabilities.
  * @return 0 on success, negative errno on error
  */
-static int camera_op_capabilities(struct device *dev, uint32_t *size,
-                                  uint8_t *capabilities)
+static int camera_op_capabilities(struct device *dev, size_t *size,
+                                  const uint8_t **caps)
 {
-    /* init capabilities [Fill in fake value]*/
-    capabilities[0] = CAP_METADATA_GREYBUS;
-    capabilities[0] |= CAP_METADATA_MIPI;
-    capabilities[0] |= CAP_STILL_IMAGE;
-    capabilities[0] |= CAP_JPEG;
+    /* TODO: Not implemented yet. */
+    *capabilities = NULL;
+    *size = 0;
 
-    *size = sizeof(uint32_t);
     return 0;
-}
-
-/**
- * @brief Get required data size of camera module information
- * @param dev Pointer to structure of device data
- * @param capabilities Pointer that will be stored Camera Module capabilities.
- * @return 0 on success, negative errno on error
- */
-static int camera_op_get_required_size(struct device *dev, uint8_t operation,
-                                       uint16_t *size)
-{
-    switch (operation) {
-    case SIZE_CAPABILITIES:
-        *size = 16;
-        return 0;
-    default:
-        return -EINVAL;
-    }
 }
 
 /**
@@ -1365,7 +1344,6 @@ static void camera_dev_remove(struct device *dev)
 
 static struct device_camera_type_ops camera_type_ops = {
     .capabilities       = camera_op_capabilities,
-    .get_required_size  = camera_op_get_required_size,
     .set_streams_cfg    = camera_op_set_streams_cfg,
     .capture            = camera_op_capture,
     .flush              = camera_op_flush,

commit 023f723548f8e313165bc098713e3799ce10e02c

white-camera: Fix compilation error
Commit 7e0502d9e425 ("white-camera: Update to the latest NuttX camera
capabilities API") renamed a function parameter but missed one location
where the parameter was used. Fix it.

Testing Done: Compile-tested
Bug Number: Trivial
Review URL: https://reviews.leaflabs.com/r/2944/
Reviewed by: fparent, jmondi

Fixes: 7e0502d9e425 ("white-camera: Update to the latest NuttX camera capabilities API")
Signed-off-by: Laurent Pinchart <laurent.pinchart@linaro.org>

module/white-camera/board.c
@@ -1037,7 +1037,7 @@ static int camera_op_capabilities(struct device *dev, size_t *size,
                                   const uint8_t **caps)
 {
     /* TODO: Not implemented yet. */
-    *capabilities = NULL;
+    *caps = NULL;
     *size = 0;
 
     return 0;